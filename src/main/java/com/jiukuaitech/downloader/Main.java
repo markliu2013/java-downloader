7b9eaee104cd8e62cbef0816444e5282e062f432
markliu2013 <markliu2013@gmail.com>
2022-07-21 16:23:49 +0800

    save

---
+++ src/main/java/com/jiukuaitech/downloader/Main.java
@@ -0,0 +1,11 @@
+package com.jiukuaitech.downloader;
+
+import com.jiukuaitech.downloader.ui.Index;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Index();
+    }
+
+}
---
+++ src/main/java/com/jiukuaitech/downloader/ui/Index.java
@@ -0,0 +1,15 @@
+package com.jiukuaitech.downloader.ui;
+
+import javax.swing.*;
+
+public class Index {
+
+    private JFrame frame;
+
+    public Index() {
+        frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+
+}
